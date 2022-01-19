ceb98bce4c7fd1ecfc53d46390b46544a84d0b7a
aliterm <me@situsali.com>
2022-01-19 14:50:55 +0700

    Add resource: Get Balance

--- lib/xendit/resources.rb
+++ lib/xendit/resources.rb
@@ -1,3 +1,4 @@
 # frozen_string_literal: true
 
 require_relative 'resources/invoice'
+require_relative 'resources/balance'
---
+++ lib/xendit/resources/balance.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'faraday'
+require 'json'
+
+require_relative '../api_operations'
+require_relative '../response'
+
+module Xendit
+  class Balance
+    class << self
+      def get()
+        resp = Xendit::APIOperations.get "balance"
+        Xendit::Response.handle_error_response resp
+        JSON.parse resp.body
+      end
+    end
+  end
+end
